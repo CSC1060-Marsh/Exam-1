d2c5c803ee8647d5974c309120f95100c169af6d
CSC1060-Marsh <143460025+CSC1060-Marsh@users.noreply.github.com>
2023-09-25 11:04:03 -0600

    Added Third Program

---
+++ src/TenMaker.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TenMaker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the first integer.");
+        //This defines the double "firstInt" as their input. I chose a double here so that they can enter a number with multiple digits.
+        double firstInt = sc.nextDouble();
+        System.out.println("Please enter the second integer.");
+        double secondInt = sc.nextDouble();
+    }
+
+    public static int makes10()
+}
