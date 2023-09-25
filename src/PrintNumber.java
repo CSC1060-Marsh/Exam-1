4cfe6e2fdb95810d6c7af80a889d57231276d123
CSC1060-Marsh <143460025+CSC1060-Marsh@users.noreply.github.com>
2023-09-25 11:15:35 -0600

    Added Fourth Program

---
+++ src/PrintNumber.java
@@ -0,0 +1,8 @@
+public class PrintNumber {
+    public static void main(String[] args) {
+        int number = 5;
+        for (number = 5; number <= 19; number++) {
+            System.out.println(number);
+            }
+    }
+}
